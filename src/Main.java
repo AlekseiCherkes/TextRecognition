ec0b4f6ac18d2237547f1548c752fdc394e66d96
aleksei.cherkes <aleksei.cherkes@5f9f9050-f221-11dd-ada4-5153b8187bf2>
2009-02-07 11:57:52 +0000

    Сделана первая компилируемая версия в ветке scons.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
-import pkg;
+import pkg.*;
 
 public class Main 
 {
     public static void main( String[] args )
     {
-//         Test test = Test();
+        Test test = new Test();
 
-//         System.out.println("Hello, world!\n");
-//         System.out.println("Invoke Test.foo(): " + test.foo(1, 2));
+        System.out.println("Hello, world!\n");
+        System.out.println("Invoke Test.foo(): " + test.foo(1, 2));
     }
 }
