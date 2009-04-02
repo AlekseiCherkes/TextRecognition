2f0e9671b87e2865b8e95c502d04c31b3d398016
aleksei.cherkes <aleksei.cherkes@5f9f9050-f221-11dd-ada4-5153b8187bf2>
2009-04-02 11:15:42 +0000

    Better scaling images and refactoring of View.java.

--- src/TextRecognition/src/View.java
+++ src/TextRecognition/src/View.java
@@ -1,11 +1,6 @@
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
 
-import java.awt.image.BufferedImage;
-
-import processing.Greyscale;
-import processing.Binarization;
-
 public class View extends QWidget {
     public Signal1<Boolean> valid = new Signal1<Boolean>();
 
@@ -31,51 +26,22 @@ public class View extends QWidget {
     }
 
     public QImage image() {
-        return original;
+        return source_original;
     }
 
     public void setImage(QImage original) {
-        this.original = original != null ? original.convertToFormat(QImage.Format.Format_ARGB32_Premultiplied) : null;
+        this.source_original = original != null ? original.convertToFormat(QImage.Format.Format_ARGB32_Premultiplied) : null;
         resetImage();
 
         valid.emit(original != null);
     }
 
     public QImage modifiedImage() {
-        return modified;
-    }
-
-    public void setColorBalance(int c) {
-        colorBalance = c;
-        resetImage();
-    }
-
-    public void setRedCyan(int c) {
-        redCyan = c;
-        resetImage();
-    }
-
-    public void setGreenMagenta(int c) {
-        greenMagenta = c;
-        resetImage();
-    }
-
-    public void setBlueYellow(int c) {
-        blueYellow = c;
-        resetImage();
-    }
-
-    public void setInvert(boolean b) {
-        invert = b;
-        resetImage();
+        return source_modified;
     }
 
     public QSize sizeHint() {
         return new QSize(500, 500);
-    }
-
-    private int max(int x, int y) {
-        return x > y ? x : y;
     }
 
     protected void paintEvent(QPaintEvent e) {
@@ -93,54 +59,59 @@ public class View extends QWidget {
         QPainter p = new QPainter(this);
         p.drawPixmap(0, 0, background);
 
-        if (modified == null)
+        if (source_modified == null)
             updateImage();
 
-        if (modified != null && !modified.isNull()) {
+        if (source_modified != null && !source_modified.isNull()) {
             p.setViewport(rect().adjusted(10, 10, -10, -10));
-//            QRect rect = rectForImage(modified);
-//      //    p.setRenderHint(QPainter.RenderHint.SmoothPixmapTransform);
-//            p.drawImage(rect, modified);
-//            p.drawImage(0, height() - rmodified.height() + 10, rmodified);
-//
-//            p.drawImage(0, height() - grayscaled.height() + 10, grayscaled);
-//            p.drawImage(0, height() - binarized.height() + 10, binarized);
-//
-//            p.drawText(rect(), Qt.AlignmentFlag.AlignCenter.value(), "Qt");
 
-            int mw = max(modified.width(), rmodified.width());
-            int mh = max(modified.height(), rmodified.height());
+            int w = source_modified.width();
+            int h = source_modified.height();
 
             QSize size = size();
-            QSize isize = new QSize(mw, mh);
+            QSize isize_v = new QSize(w, 2 * h);
+            QSize isize_h = new QSize(2 * w, h);
 
-            isize.scale(size, Qt.AspectRatioMode.KeepAspectRatio);
+            isize_v.scale(size, Qt.AspectRatioMode.KeepAspectRatio);
+            isize_h.scale(size, Qt.AspectRatioMode.KeepAspectRatio);
 
-            QRect rect = new QRect(width() / 2 - isize.width() / 2,
-                    size.height() / 2 - isize.height() / 2,
-                    isize.width(), isize.height());
+            QRect rect1 = new QRect();
+            QRect rect2 = new QRect();
 
-            int w = rect.width() / 3;
-            int h = rect.height() / 3;
+            if (isize_h.width() * isize_h.height() >
+                    isize_v.width() * isize_v.height()) {
 
-            int dw = 10;
-            int dh = 10;
+                QSize isize = isize_h;
+                w = isize.width() / 2;
+                h = isize.height();
 
-            QRect rect1 = new QRect(rect.x() + 0 * w + dw, rect.y() + dh, w - dw, h - dh);
-            QRect rect2 = new QRect(rect.x() + 1 * w + dw, rect.y() + dh, w - dw, h - dh);
-            QRect rect3 = new QRect(rect.x() + 2 * w + dw, rect.y() + dh, w - dw, h - dh);
+                QRect rect = new QRect(width() / 2 - isize.width() / 2,
+                        size.height() / 2 - isize.height() / 2,
+                        isize.width(), isize.height());
 
-//            QRect rrect1 = new QRect(rect.x() + 0 * w + dw, rect.y() + h + dh, w - dw, h - dh);
-//            QRect rrect2 = new QRect(rect.x() + 1 * w + dw, rect.y() + h + dh, w - dw, h - dh);
-//            QRect rrect3 = new QRect(rect.x() + 2 * w + dw, rect.y() + h + dh, w - dw, h - dh);
+                int dw = (int) (0.1 * w);
+                int dh = (int) (0.1 * h);
 
-            p.drawImage(rect1, modified);
-            p.drawImage(rect2, grayscaled);
-            p.drawImage(rect3, binarized);
+                rect1 = new QRect(rect.x() + dw, rect.y() + dh, w - 2 * dw, h - 2 * dh);
+                rect2 = new QRect(rect.x() + w + dw, rect.y() + dh, w - 2 * dw, h - 2 * dh);
+            } else {
+                QSize isize = isize_v;
+                w = isize.width();
+                h = isize.height() / 2;
 
-//            p.drawImage(rrect1, rmodified);
-//            p.drawImage(rrect2, rgrayscaled);
-//            p.drawImage(rrect3, rbinarized);
+                QRect rect = new QRect(width() / 2 - isize.width() / 2,
+                        size.height() / 2 - isize.height() / 2,
+                        isize.width(), isize.height());
+
+                int dw = (int) (0.1 * w);
+                int dh = (int) (0.1 * h);
+
+                rect1 = new QRect(rect.x() + dw, rect.y() + dh, w - 2 * dw, h - 2 * dh);
+                rect2 = new QRect(rect.x() + dw, rect.y() + h + dh, w - 2 * dw, h - 2 * dh);
+            }
+
+            p.drawImage(rect1, source_modified);
+            p.drawImage(rect2, source_modified);
         }
 
         p.end();
@@ -156,9 +127,9 @@ public class View extends QWidget {
     }
 
     private final void resetImage() {
-        if (modified != null)
-            modified.dispose();
-        modified = null;
+        if (source_modified != null)
+            source_modified.dispose();
+        source_modified = null;
         delayedUpdate.start();
     }
 
@@ -169,126 +140,24 @@ public class View extends QWidget {
     }
 
     private void updateImage() {
-        if (original == null || original.isNull())
+        if (source_original == null || source_original.isNull())
             return;
 
-        if (modified != null)
-            modified.dispose();
+        if (source_modified != null)
+            source_modified.dispose();
 
-        modified = original.copy();
+        source_modified = source_original.copy();
 
         QPainter p = new QPainter();
-        p.begin(modified);
+        p.begin(source_modified);
         p.setCompositionMode(QPainter.CompositionMode.CompositionMode_SourceAtop);
-        if (redCyan != 0) {
-            QColor c = decideColor(redCyan, QColor.cyan, QColor.red);
-            p.fillRect(0, 0, modified.width(), modified.height(), new QBrush(c));
-        }
-        if (greenMagenta != 0) {
-            QColor c = decideColor(greenMagenta, QColor.magenta, QColor.green);
-            p.fillRect(0, 0, modified.width(), modified.height(), new QBrush(c));
-        }
-        if (blueYellow != 0) {
-            QColor c = decideColor(blueYellow, QColor.yellow, QColor.blue);
-            p.fillRect(0, 0, modified.width(), modified.height(), new QBrush(c));
-        }
-        if (colorBalance != 0) {
-            QColor c = decideColor(colorBalance, QColor.white, QColor.black);
-            p.fillRect(0, 0, modified.width(), modified.height(), new QBrush(c));
-        }
-
-        if (invert) {
-            p.setCompositionMode(QPainter.CompositionMode.CompositionMode_Difference);
-            p.fillRect(modified.rect(), new QBrush(QColor.white));
-        }
-
         p.end();
-
-        grayscaled = Greyscale.work(modified);
-        binarized = Binarization.work(modified);
-        rmodified = createReflection(modified);
-        rgrayscaled = createReflection(grayscaled);
-        rbinarized = createReflection(binarized);
     }
 
-    private QRect rectForImage(QImage image) {
-        QSize isize = image.size();
-        QSize size = size();
-
-        size.setHeight(size.height() * 3 / 4);
-        size.setWidth(size.width() * 3 / 4);
-
-        isize.scale(size, Qt.AspectRatioMode.KeepAspectRatio);
-
-        return new QRect(width() / 2 - isize.width() / 2,
-                size.height() / 2 - isize.height() / 2,
-                isize.width(), isize.height());
-    }
-
-    private QImage createReflection(QImage source) {
-        if (source == null || source.isNull())
-            return null;
-
-        QRect r = rectForImage(source);
-
-        QImage image = new QImage(width(),
-                height() - r.height() - r.y(),
-                QImage.Format.Format_ARGB32_Premultiplied);
-        image.fill(0);
-
-        double iw = image.width();
-        double ih = image.height();
-
-        QPainter pt = new QPainter(image);
-
-        pt.setRenderHint(QPainter.RenderHint.SmoothPixmapTransform);
-        pt.setRenderHint(QPainter.RenderHint.Antialiasing);
-
-        pt.save();
-        {
-            QPolygonF imageQuad = new QPolygonF();
-            imageQuad.add(0, 0);
-            imageQuad.add(0, source.height());
-            imageQuad.add(source.width(), source.height());
-            imageQuad.add(source.width(), 0);
-            QPolygonF targetQuad = new QPolygonF();
-            targetQuad.add(0, ih);
-            targetQuad.add(iw / 2 - r.width() / 2, 0);
-            targetQuad.add(iw / 2 + r.width() / 2, 0);
-            targetQuad.add(iw, ih);
-            try {
-                pt.setTransform(QTransform.quadToQuad(imageQuad, targetQuad));
-                pt.drawImage(imageQuad.boundingRect(), source);
-            } catch (IllegalArgumentException e) {
-                // user has resized the view too small
-            }
-        }
-        pt.restore();
-
-        QLinearGradient lg = new QLinearGradient(0, 0, 0, image.height());
-        lg.setColorAt(0.1, QColor.fromRgbF(0, 0, 0, 0.4));
-        lg.setColorAt(0.6, QColor.transparent);
-        pt.setCompositionMode(QPainter.CompositionMode.CompositionMode_DestinationIn);
-        pt.fillRect(image.rect(), new QBrush(lg));
-        pt.end();
-
-        return image;
-    }
-
-    private int colorBalance;
-    private int redCyan;
-    private int greenMagenta;
-    private int blueYellow;
-
-    private boolean invert;
-
-    private QImage original;
-    private QImage modified;
-    private QImage grayscaled;
-    private QImage binarized;
-    private QImage rmodified;
-    private QImage rgrayscaled;
-    private QImage rbinarized;
+    private QImage source_original;
+    private QImage source_modified;
+    private QImage processed_original;
+    private QImage processed_modified;
     private QPixmap background;
 
     private Worker delayedUpdate = new Worker(this) {
